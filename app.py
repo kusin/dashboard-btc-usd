74b05df4192bb375cebddc00432c6b7c112be055
kusin <aryajayaalamsyah@gmail.com>
2022-12-31 00:30:09 +0700

    -

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ import numpy as np;
 
 # library data visualization
 import plotly.express as px;
+import plotly.graph_objects as go;
+
+# import function any file .py
+from func_plot import plot_line;
 
 # set config ui-dasboard streamlit
 st.set_page_config(
@@ -23,25 +27,36 @@ st.set_page_config(
     }
 );
 
+# load dataset
+dataset = pd.read_csv("dataset/btc-usd.csv", parse_dates=["Date"]);
+
 # container-header
 with st.container():
-    st.markdown("## Comparison algorithm LSTM and GRU use architecture stacked and bidirectional");
+    st.markdown("## Comparison LSTM and GRU use architecture stacked and bidirectional");
 
 # container-dataset
 with st.container():
 
     # set columns with col-3 row-1
-    col1, col2, col3 = st.columns(3);
+    col1, col2, col3, col4 = st.columns(4);
 
     # columns col-1
-    col1.selectbox('choose dataset',('BTC-USD','IBM'));
+    col1.selectbox("choose algorithm",("LSTM-RNN", "GRU-RNN"));
 
     # columns col-2
-    col2.date_input("start date");
+    col2.selectbox("choose dataset",("BTC-USD", "IBM"));
 
     # columns col-3
-    col3.date_input("end date");
+    col3.date_input("start date");
+
+    # columns col-4
+    col4.date_input("end date");
 
 # container-show-dataset
-# with st.container():
+with st.container():
+    st.subheader("Data visualization time series");
 
+    # plot line sst nina 3.4
+    st.plotly_chart(
+        plot_line(dataset, "Date", "Open", "#3457D5"), use_container_width=True
+    );
---
+++ func_plot.py
@@ -0,0 +1,25 @@
+# library ui-dashboard
+import streamlit as st;
+
+# library manipulation dataset
+import pandas as pd;
+
+# library manipulation array
+import numpy as np;
+
+# library data visualization
+import plotly.express as px;
+import plotly.graph_objects as go;
+
+# func plot line with plotly graph objects
+def plot_line(data, value_x, value_y, nm_color):
+    
+    # show plot
+    fig = go.Figure(
+        data = [
+            go.Scatter(x=data[value_x], y=data[value_y], line_color=nm_color)
+        ]
+    );
+    
+    # return value
+    return fig;
