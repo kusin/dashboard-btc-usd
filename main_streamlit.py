772475a00f37dd49487afbfe44879314ee20db35
kusin <aryajayaalamsyah@gmail.com>
2024-09-02 08:14:33 +0700

    -

--- main_streamlit.py
+++ main_streamlit.py
@@ -1,10 +1,10 @@
 # set library
 import streamlit as st
-import datetime as dt
 
 # import from other file
 from C01_data_collection import *
 from C02_visualization import *
+from main_function import *
 # --------------------------------------------------------------------------------------------------------------
 
 # set random number
@@ -32,11 +32,7 @@ col1, col2, col3 = st.columns([0.25, 0.25, 0.5], gap="small")
 # columns form dataset
 with col1:
   st.info("Config Dataset")
-  with st.form("my-form1"):
-    cryptocurrency = st.selectbox(label="Choose a cryptocurrency", options=("BTC-USD", "ETH-USD"))
-    start = st.date_input(label="Start Date", value=dt.date(2015,1,1))
-    end = st.date_input(label="End Date", value=dt.date(2024,4,30))
-    process = st.form_submit_button(label="Process", type="secondary", use_container_width=True)
+  cryptocurrency, start, end, process = form_dataset()
 # --------------------------------------------------------------------------------------------------------------
 
 # columns - information crypto
@@ -61,10 +57,43 @@ with col3:
     startDate   = "2015-01-01"
     endDate     = "2024-05-01"
   dataset = getData(ticker, startDate, endDate)
-  # -----------------------------------------------
 
   # split two tab-index
   tab1, tab2 = st.tabs(["Visualization Data", "Historical Data"])
   tab1.plotly_chart(timeseries_plot(dataset), use_container_width=True)
   tab2.dataframe(dataset, use_container_width=True)
-# ------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------
+
+# # split three columns
+# col1, col2, col3 = st.columns([0.25, 0.25, 0.5], gap="small")
+
+# columns - form hyperparameter
+with col1:
+  st.info("Hyperparameter Tuning")
+  algorithms, optimizers, batch_size, epoch, submit = form_hyperparameter()
+# --------------------------------------------------------------------------------------------------------------
+
+# columns - evaluation models
+with col2:
+  st.info("Evaluation Models")
+  if submit:
+    process_prediction()
+# --------------------------------------------------------------------------------------------------------------
+
+# columns - results prediction
+with col3:
+  st.info("Results Prediction")
+  if submit:
+
+  #   # results prediction
+  #   results = pd.concat([
+  #     pd.DataFrame(np.array(dataset[["Date"]].iloc[len(y_train)+120:]), columns=["Date"]),
+  #     pd.DataFrame(np.array(inverse(scaler=scaler,scaled=y_test)), columns=["Actual"]),
+  #     pd.DataFrame(np.array(inverse(scaler=scaler,scaled=predictions)), columns=["Predictions"])
+  #   ],axis=1)
+
+    # split two tab-index
+    tab1, tab2 = st.tabs(["Visualization Data", "Historical Data"])
+    tab1.plotly_chart(timeseries_plot(dataset), use_container_width=True)
+    tab2.dataframe(dataset, use_container_width=True)
+# --------------------------------------------------------------------------------------------------------------
